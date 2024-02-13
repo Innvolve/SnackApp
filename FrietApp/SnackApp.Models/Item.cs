52a91aef984b9bea5d7c6f48fa283288f0610497
MichaelA <72653291+MichaelAanraad@users.noreply.github.com>
2024-02-13 10:51:52 +0100

    added item properties and instantiation

--- FrietApp/SnackApp.Models/Item.cs
+++ FrietApp/SnackApp.Models/Item.cs
@@ -2,5 +2,12 @@ namespace SnackApp.Models;
 
 public class Item
 {
-    
+    public string Name { get; init; }
+    public string? Description { get; init; } = string.Empty;
+    public string ImgUrl { get; init; }
+    public double Price { get; init; }
+    public string? Options { get; init; }
+    public string? Additives { get; init; }
+    public string? Size { get; init; }
+    public bool Availability { get; set; } = true;
 }
--- FrietApp/SnackApp/Program.cs
+++ FrietApp/SnackApp/Program.cs
@@ -1,21 +1,54 @@
 ﻿using HtmlAgilityPack;
-using ScrapySharp;
+using HtmlAgilityPack.CssSelectors.NetCore;
 using ScrapySharp.Extensions;
 using ScrapySharp.Network;
+using SnackApp.Models;
 
 
 //source: https://zenscrape.com/web-scraping-csharp/
 var scrapingBrowser = new ScrapingBrowser();
-var baseDomain = "https://cafetariabienvenue.12waiter.eu";
+const string baseDomain = "https://cafetariabienvenue.12waiter.eu";
 
-
+//Get product links
 var collectionLinks = GetCollectionLinks(baseDomain);
 var productLinks = GetProductLinks(collectionLinks);
+
+// Create Items
 foreach (var link in productLinks)
 {
-    Console.WriteLine(link);
-}
+    var productlink = $"{baseDomain}{link}";
+    var html = GetHtml(productlink);
+    var node = html.OwnerDocument.DocumentNode;
+    var items = new List<Item>();
+    try
+    {
+        items.Add(new Item
+        {
+            Name = node.QuerySelector("h1").InnerText,
+            Description = node.QuerySelector("div.product-section.product-intro > p").InnerText ?? "N/A",
+            ImgUrl = node.QuerySelector("img").ChildAttributes("src").FirstOrDefault()?.Value ?? "N/A",
+            Price = 0, //node.SelectSingleNode("//*[@id=\"product-form\"]/fieldset/div/div[1]").InnerText,
+            // Options = node.QuerySelector("").InnerText,
+            // Additives = node.QuerySelector("").InnerText,
+            // Size = node.QuerySelector("").InnerText,
+            // Availability = true //node.SelectSingleNode("").InnerText;
+        });
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        throw;
+    }
+    
+    foreach (var item in items)
+    {
+        Console.WriteLine(item.Name);
+        Console.WriteLine(item.Description);
+        Console.WriteLine(item.ImgUrl);
+        
+    }
 
+}
 
 //Methods
 List<string> GetCollectionLinks(string url)
@@ -53,7 +86,7 @@ List<string> GetProductLinks(List<string> collectionUrls)
                 }
             }
         }
-        foreach (var link in links)
+        foreach (var link in links) 
         {
             productLinks.Add(link.Attributes["href"].Value);
         }
