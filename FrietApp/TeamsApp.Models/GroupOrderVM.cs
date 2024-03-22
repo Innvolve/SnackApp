5b8eef4607c26d5f7f79166601cd5e10c6b0eed3
MichaelA <72653291+MichaelAanraad@users.noreply.github.com>
2024-03-22 16:06:18 +0100

    Added parent child callbacks

--- FrietApp/TeamsApp.Models/GroupOrderVM.cs
+++ FrietApp/TeamsApp.Models/GroupOrderVM.cs
@@ -1,4 +1,5 @@
 using Entities;
+using SnackApp.View.Models;
 
 namespace SnackApp.Models;
 
@@ -9,12 +10,24 @@ public class GroupOrderVM
     public List<OrderVM> Orders { get; }
     public Currency PriceTotal { get; }
 
+    public GroupOrderVM()
+    {
+        Owner = "TestOwner"; //TODO: get the owner from the logged in user
+        Date = DateTime.Now;
+        Orders = new List<OrderVM>();
+        PriceTotal =  new Currency
+        {
+            CurrencySymbol = Orders.FirstOrDefault()?.OrderPrice.CurrencySymbol ?? string.Empty,
+            Value = Orders.Sum(item => item.OrderPrice.Value)
+        };
+
+    }
     public GroupOrderVM(GroupOrder groupOrder)
     {
         Owner = groupOrder.Owner;
         Date = groupOrder.Date;
         Orders = new List<OrderVM>(groupOrder.Orders.Select(order => new OrderVM(order)));
-        PriceTotal = groupOrder.PriceTotal;
+        PriceTotal = groupOrder.PriceTotal; //TODO: Test if this price gets updated
     }
   
     public void AddOrderToGroupOrder(OrderVM order)
